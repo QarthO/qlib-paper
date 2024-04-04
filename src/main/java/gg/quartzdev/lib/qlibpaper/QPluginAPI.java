0022a6eeb456140cdbdb3085cfe23f34fcd49cc4
QarthO <qartho@gmail.com>
2024-04-04 18:17:19 -0400

    switched api to interface

--- src/main/java/gg/quartzdev/lib/qlibpaper/QPluginAPI.java
+++ src/main/java/gg/quartzdev/lib/qlibpaper/QPluginAPI.java
@@ -1,25 +1,9 @@
 package gg.quartzdev.lib.qlibpaper;
-
-import org.bstats.bukkit.Metrics;
-import org.bukkit.plugin.java.JavaPlugin;
-
-public abstract class QPluginAPI {
-    private JavaPlugin pluginInstance;
-
-    public void setupMetrics(int pluginId){
-        new Metrics(pluginInstance, pluginId);
-    }
-
-    @SuppressWarnings("UnstableApiUsage")
-    public String getVersion(){
-        return pluginInstance.getPluginMeta().getVersion();
-    }
-
-    public String getName(){
-        return pluginInstance.getName();
-    }
-
-    public abstract void setupConfig();
-    public abstract void registerListeners();
-    public abstract void registerCommands();
+public interface QPluginAPI {
+    String getVersion();
+    String getName();
+    void setupMetrics();
+    void setupConfig();
+    void registerListeners();
+    void registerCommands();
 }
